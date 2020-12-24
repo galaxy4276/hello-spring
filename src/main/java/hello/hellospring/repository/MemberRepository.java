518a88cd2059a71a8858a30e9cfdf206a52987ca
galaxy <galaxyhi4276@gmail.com>
2020-12-24 18:39:13 +0900

    feat: 서회원 관련 서비스, 리포지토리 ê°작성

---
+++ src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -0,0 +1,17 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+  Member save(Member member);
+  Optional<Member> findById(Long id);
+  Optional<Member> findByName(String name);
+  List<Member> findAll();
+  /*
+    Optional 은 자바 8에서 나온 기능이다.
+    null 값에 대해서 null을 그대로 반환하는 대신 Optional을 감싸서 반환하는 방법을 선호한다.
+   */
+}
